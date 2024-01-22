b3b6a1118df396851578845ac6e8d495605319c1
Tom Yachil <yachil@post.bgu.ac.il>
2024-01-22 19:33:58 +0200

    Updated skeleton header files

--- include/WareHouse.h
+++ include/WareHouse.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <string>
 #include <vector>
-using namespace std;
 
 #include "Order.h"
 #include "Customer.h"
@@ -17,13 +16,12 @@ class WareHouse {
     public:
         WareHouse(const string &configFilePath);
         void start();
-        const vector<BaseAction*> &getActionsLog() const;
         void addOrder(Order* order);
         void addAction(BaseAction* action);
-        void printActionsLogs();
         Customer &getCustomer(int customerId) const;
         Volunteer &getVolunteer(int volunteerId) const;
         Order &getOrder(int orderId) const;
+        const vector<BaseAction*> &getActions() const;
         void close();
         void open();
 
