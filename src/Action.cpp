7e816244f637ed759c7d0a2f4235038aca6fa655
Tom Yachil <yachil@post.bgu.ac.il>
2024-01-24 23:38:00 +0200

    continued Actions

--- src/Action.cpp
+++ src/Action.cpp
@@ -33,12 +33,13 @@ string BaseAction::getStatusToString() const
 }
 
 SimulateStep::SimulateStep(int numOfSteps) : numOfSteps(numOfSteps) {}
-SimulateStep::SimulateStep(const SimulateStep &other): numOfSteps(numOfSteps) {}
+SimulateStep::SimulateStep(const SimulateStep &other): numOfSteps(other.numOfSteps) {}
 
 void SimulateStep::act(WareHouse &wareHouse)
 {
     wareHouse.addAction(this);
     //TO DO Figure out what to do
+    complete();
 }
 
 string SimulateStep::toString() const
@@ -65,6 +66,7 @@ void AddOrder::act(WareHouse &wareHouse)
         customer.addOrder(orderId);
         Order *order = new Order(orderId, customerId, customer.getCustomerDistance());
         wareHouse.addOrder(order);
+        complete();
     }
     else
     {
@@ -93,6 +95,7 @@ void AddCustomer::act(WareHouse &wareHouse)
     wareHouse.addAction(this);
     Customer *customer = createCustomer(wareHouse.getCustomerCounter());
     wareHouse.addCustomer(customer);
+    complete();
 }
 
 AddCustomer *AddCustomer::clone() const
@@ -121,20 +124,65 @@ PrintOrderStatus::PrintOrderStatus(int id) : orderId(id) {}
 
 void PrintOrderStatus::act(WareHouse &wareHouse)
 {
-    Order order = wareHouse.getOrder(orderId);
+    Order &order = wareHouse.getOrder(orderId);
     std::cout << "OrderId: " + std::to_string(order.getId()) 
     + "\nOrderStatus: " + order.statusToString()
     + "\nCustomerID: " + std::to_string(order.getCustomerId()) 
     + "\nCollector: " + std::to_string(order.getCollectorId())
     + "\nDriver: " + std::to_string(order.getDriverId())<< std::endl;
+    complete();
 }
 
 PrintOrderStatus *PrintOrderStatus::clone() const
 {
-    return nullptr;
+    return new PrintOrderStatus(*this);
 }
 
 string PrintOrderStatus::toString() const
+{
+    return "printOrderStatus " + std::to_string(orderId) + getStatusToString();
+}
+
+PrintCustomerStatus::PrintCustomerStatus(int customerId): customerId(customerId) {}
+
+void PrintCustomerStatus::act(WareHouse &wareHouse)
+{
+    Customer &customer = wareHouse.getCustomer(customerId);
+    std::cout << "CustomerID: " + std::to_string(customer.getId()) << std::endl;
+    vector<int> orderIds = customer.getOrdersIds();
+    for (int id : orderIds)
+    {
+        Order order = wareHouse.getOrder(id);
+        std::cout << "OrderID: " + std::to_string(id)
+        + "\nOrderStatus: " + order.statusToString() << std::endl;
+    }
+    std::cout << "numOrdersLeft: " + std::to_string(customer.getMaxOrders() - customer.getNumOrders()) << std::endl; 
+    complete();
+}
+
+PrintCustomerStatus *PrintCustomerStatus::clone() const
+{
+    return new PrintCustomerStatus(*this);
+}
+
+string PrintCustomerStatus::toString() const
+{
+    return "printCustomerStatus: " + std::to_string(customerId) + " " + getStatusToString();
+}
+
+PrintVolunteerStatus::PrintVolunteerStatus(int id) : volunteerId(id) {}
+
+void PrintVolunteerStatus::act(WareHouse &wareHouse) 
+{
+    
+}
+
+PrintVolunteerStatus *PrintVolunteerStatus::clone() const
+{
+    return nullptr;
+}
+
+string PrintVolunteerStatus::toString() const
 {
     return string();
 }
